2da7054b2aba642f0006ed8075fed68d81bc4343
MikaelReichiSopany <111833130+MikaelReichiSopany@users.noreply.github.com>
2022-12-05 04:38:23 +0700

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -2,10 +2,10 @@ process.env.Node_ENV = (process.env.Node_ENV || 'development').toLowerCase();
 
 const dotenv = require('dotenv');
 
-const envFound = dotenv.config({ path: './config/.env' });
-if (envFound.error) {
-  throw new Error('File.env not found.');
-}
+// const envFound = dotenv.config({ path: './config/.env' });
+// if (envFound.error) {
+//   throw new Error('File.env not found.');
+// }
 
 module.exports = {
   port: process.env.PORT || 5000,
